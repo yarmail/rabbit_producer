36d9710c08dea3befbd2151ef4bb7e98eb02cf0b
yarmail <yarmail@yandex.ru>
2023-07-26 00:51:03 +0300

    update

--- src/main/java/com/example/rabbit_producer/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbit_producer/config/RabbitMQConfig.java
@@ -8,7 +8,6 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
