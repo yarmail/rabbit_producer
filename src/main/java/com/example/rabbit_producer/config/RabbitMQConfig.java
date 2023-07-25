8b0aca810f8658d2516eae4eefc22733b9e5df2d
yarmail <yarmail@yandex.ru>
2023-07-25 15:03:27 +0300

    update

--- src/main/java/com/example/rabbit_producer/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbit_producer/config/RabbitMQConfig.java
@@ -41,6 +41,7 @@ public class RabbitMQConfig {
         cachingConnectionFactory.setUsername(username);
         cachingConnectionFactory.setPassword(password);
         cachingConnectionFactory.setPort(port);
+        cachingConnectionFactory.setCacheMode(CachingConnectionFactory.CacheMode.CONNECTION);
         return cachingConnectionFactory;
     }
 
