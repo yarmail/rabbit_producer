adb9f3933e274c8294a6b76780856ff59a4fdb9f
yarmail <yarmail@yandex.ru>
2023-07-25 04:34:26 +0300

    update

--- src/main/java/com/example/rabbit_producer/config/RabbitMQConfig.java
+++ src/main/java/com/example/rabbit_producer/config/RabbitMQConfig.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,7 @@ public class RabbitMQConfig {
     private String username;
     @Value("${spring.rabbitmq.password}")
     private String password;
-    @Value("${spring.rabbitmq.user.virtualHost}")
+    @Value("${spring.rabbitmq.virtualHost}")
     private String virtualHost;
     @Value("${spring.rabbitmq.port}")
     private int port;
--- src/test/java/com/example/rabbit_producer/RabbitProducerApplicationTests.java
+++ src/test/java/com/example/rabbit_producer/RabbitProducerApplicationTests.java
@@ -5,4 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RabbitProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
 }
